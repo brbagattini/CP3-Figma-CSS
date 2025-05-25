219968a58ff5452b58b31a95b1e169effd11615b
Bruno Bagattini Fernandes <brbagattinif@gmail.com>
2025-05-25 19:45:14 -0300

    Feat: adição do menu e creditos e @media

---
+++ Js/app.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menu-toggle');
+const menuList = document.getElementById('menu-list');
+
+menuToggle.addEventListener('click', () => {
+  menuList.classList.toggle('active');
+});
