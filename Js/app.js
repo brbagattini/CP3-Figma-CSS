c334e0964892b4137d3138752e1d63c4904ecdf2
Bruno Bagattini Fernandes <brbagattinif@gmail.com>
2025-05-25 22:29:13 -0300

    fix: remoção do leia mais

--- Js/app.js
+++ Js/app.js
@@ -8,21 +8,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         behavior: 'smooth'
       });
     }
-
-    // Fecha o menu no mobile após o clique
     document.getElementById('menu-list').classList.remove('active');
   });
-});
-document.getElementById("leiamais").addEventListener("click", function (e) {
-  e.preventDefault();
-  const conteudo = document.getElementById("conteudo-leiamais");
-
-  // Alterna visibilidade
-  if (conteudo.style.display === "none") {
-    conteudo.style.display = "block";
-    this.textContent = "Mostrar menos";
-  } else {
-    conteudo.style.display = "none";
-    this.textContent = "Leia mais";
-  }
 });
